5c7c5cb04cacc43871ab887334ef043e46591165
beini <874140704@qq.com>
2017-04-17 15:00:55 +0800

    增加多表查询

--- src/com/beini/test/javaee/PersonAndOrdersTest.java
+++ src/com/beini/test/javaee/PersonAndOrdersTest.java
@@ -3,7 +3,9 @@ package com.beini.test.javaee;
 import com.beini.bean.Orders;
 import com.beini.bean.Person;
 import com.beini.mapper.LeaderMapper;
+import com.beini.mapper.OrdersMapper;
 import com.beini.mapper.PersonMapper;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,12 +18,14 @@ import java.util.List;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/config/spring-common.xml")
-public class PersonMapperTest {
+public class PersonAndOrdersTest {
     @Autowired
     private PersonMapper personMapper;
+    @Autowired
+    OrdersMapper ordersMapper;
 
     @org.junit.Test
-    public void test() {
+    public void testPserson() {
         System.out.println("    -------->    start");
         Person personsList = personMapper.selectPersonFetchOrder(1);
         System.out.println("     --------> " + (personsList == null));
@@ -30,11 +34,23 @@ public class PersonMapperTest {
             List<Orders> ordersList = personsList.getOrderList();
             if (ordersList != null) {
                 for (Orders orders : ordersList) {
-                    System.out.println( "            --------------->"+orders.getPrice());
+                    System.out.println("            --------------->" + orders.getPrice());
                 }
             }
         }
         System.out.println("      -------->  end");
     }
 
+    @org.junit.Test
+    public void testOrders() {
+        System.out.println("    -------->    start");
+        Orders orders = ordersMapper.selectOrdersFetchPerson(1);
+        if (orders != null) {
+            System.out.println("  ----------->" + orders.getPrice());
+            if (orders.getPerson() != null) {
+                System.out.println("     "+orders.getPerson().getName());
+            }
+        }
+        System.out.println("    -------->    end");
+    }
 }
