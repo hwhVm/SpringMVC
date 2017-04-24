75867fa773aacfaced1cf922beb41e3a63249a12
beini <874140704@qq.com>
2017-04-24 14:22:46 +0800

    了解弱引用

---
+++ src/com/beini/test/javase/TestWeakReference.java
@@ -0,0 +1,77 @@
+package com.beini.test.javase;
+
+import java.lang.ref.WeakReference;
+import java.util.WeakHashMap;
+
+/**
+ * Created by beini on 2017/4/24.
+ */
+public class TestWeakReference {
+
+    public static void main(String[] args) {
+        User user = new User("beini", 33);
+        WeakReference<User> userWeakReference = new WeakReference<>(user);
+
+//
+//        int i = 0;
+//        while (true) {
+//            if (userWeakReference.get() != null) {
+//                i++;
+//                System.out.println("   userWeakReference ");
+//                user = null;
+//                System.gc();
+//            } else {
+//                System.out.println("    userWeakReference  collected");
+//                break;
+//            }
+//        }
+
+//        int i = 0;
+//        while (true) {
+//            if (user != null) {
+//                System.out.println("   user ");
+//            } else {
+//                System.out.println("    userWeakReference  collected");
+//                break;
+//            }
+//        }
+
+//        System.out.println("     i==" + i);
+
+        System.out.println("  returnString()=" + test());
+    }
+
+    private static String test() {
+        String a = new String("a");
+        WeakReference<String> b = new WeakReference<String>(a);
+        a = null;
+        System.gc();
+
+        String c = "";
+        try {
+            c = b.get().replace("a", "b");
+            return c;
+        } catch (Exception e) {
+            c = "c";
+            return c;
+        } finally {
+            c += "d";
+            return c + "e";
+        }
+    }
+
+    public static String returnString() {
+
+        try {
+            System.out.println(" try   ");
+            return "aa";
+        } catch (Exception e) {
+            System.out.println(" Exception   ");
+            return "bb";
+        } finally {
+            System.out.println(" finally   ");
+            return "finally";
+        }
+
+    }
+}
