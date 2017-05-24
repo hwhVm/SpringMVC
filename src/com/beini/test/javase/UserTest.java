bbfb25a8dbbc2d739bfe93c8a2b5e7ad2b3be3c3
beini <874140704@qq.com>
2017-05-24 13:41:14 +0800

    添加ip工具类

--- src/com/beini/test/javase/UserTest.java
+++ src/com/beini/test/javase/UserTest.java
@@ -11,22 +11,22 @@ import java.util.Objects;
 public class UserTest {
 
     public static void main(String[] args) throws InterruptedException {
-        List<User> users = new ArrayList<>();
-        users.add(new User("beini1", 11));
-        users.add(new User("beini2", 12));
-        users.add(new User("beini4", 14));
-        users.add(new User("beini3", 13));
-        Collections.sort(users);
-        users.stream().filter(Objects::nonNull).forEach(user2 -> System.out.println("    " + user2.getAge()));
-
-        User user = new User("beini3", 13);
-
-        //检索student在list中的位置
-        int index1 = users.indexOf(user);
-        int index2 = Collections.binarySearch(users, user);
-
-        System.out.println("index1 = " + index1);
-        System.out.println("index2 = " + index2);
+//        List<User> users = new ArrayList<>();
+//        users.add(new User("beini1", 11));
+//        users.add(new User("beini2", 12));
+//        users.add(new User("beini4", 14));
+//        users.add(new User("beini3", 13));
+//        Collections.sort(users);
+//        users.stream().filter(Objects::nonNull).forEach(user2 -> System.out.println("    " + user2.getAge()));
+//
+//        User user = new User("beini3", 13);
+//
+//        //检索student在list中的位置
+//        int index1 = users.indexOf(user);
+//        int index2 = Collections.binarySearch(users, user);
+//
+//        System.out.println("index1 = " + index1);
+//        System.out.println("index2 = " + index2);
 
     }
 }
